ad508ba788841a334c7fd64f6c8214fbb4fd5b84
LUXPHOS <leongf87@gmail.com>
2023-02-11 18:47:47 -0600

    Nuevo ejercicio

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        String estacion = "invierno";
+        if(estacion == "invierno"){
+            System.out.println("Es hoy, viene el gordo");
+        }
+        else if (estacion == "verano") {
+            System.out.println("Chiki Puty Babies");
+        }
+        else {
+            System.out.println("Valio verga la vida");
+        }
+
+
+
+    }
+}
